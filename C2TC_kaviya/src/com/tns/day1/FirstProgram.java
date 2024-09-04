ab2d6732cecd9c67a87e332087cc1f32399da446
ELCOT <ELCOT@kaviya>
2024-09-04 12:18:51 +0530

    This is my first commit

---
+++ C2TC_kaviya/src/com/tns/day1/FirstProgram.java
@@ -0,0 +1,46 @@
+package com.tns.day1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		int num1=50;
+
+		int num2=26;
+
+		int sum, sub,  mult,  div,  mod;
+
+		
+
+		
+
+		sum=num1+num2;
+
+		System.out.println("The Sum= " + sum);
+
+		
+
+		sub = num1 - num2;
+
+        System.out.println("Subtraction = " + sub);
+
+        
+
+        mult = num1 * num2;
+
+        System.out.println("Multiplication = " + mult);
+
+        
+
+        div = num1 / num2;
+
+        System.out.println("Division = " + div);
+
+        
+
+        mod = num1 % num2;
+
+        System.out.println("Remainder = " + mod);
+
+	}
+
+}
